9323b149e2323906988f4b7c267f07a778d93fe4
fuxiang90 <fuxiang90@gmail.com>
2013-08-04 10:46:14 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,90 @@
+#!/bin/python
+
+import re
+
+class Length(object):
+    
+
+    def __init__(self,input_file_name):
+        
+        self._mail = 'fuxiang90@gmail.com'
+        self._input_file_name = input_file_name
+        self._legth_dict ={}
+        self._unit_dict = { "miles":"mile" ,"feet":"foot","inches":"inch","yards":"yard","faths":"fath","furlong":"furlong" }
+    def run(self):
+        fin = open(self._input_file_name)
+        fout = open('output.txt','w')
+        fout.write(self._mail + '\n\n')
+        for each in fin:
+            if each.strip() == '':
+                continue
+            
+            if each.find('=') == -1:
+                result = self.compute(each.strip())
+                fout.write("%.2f m\n" %result)
+
+            else :
+                l1 = each.strip().split('=')
+                unit = l1[0].strip().split(' ')[1]
+                num  = l1[1].strip().split(' ')[0]
+            
+                self._legth_dict[unit] = self._legth_dict.get(unit ,0.0) + float(num)
+
+        fin.close()
+        fout.close()
+    def compute(self,eval_str):
+#        for each in self._legth_dict :
+#            eval_str = eval_str.replace(each )
+        eval_list = eval_str.split(' ')
+        new_eval_str = ""
+        
+        #temp_list = []
+        for pos in range(len(eval_list) ) :
+            
+            s = eval_list[pos]
+            _len = len(s)
+            
+
+            #if s[_len-1] == 's':
+            #    s = s[:_len - 1]
+            #    len_s = len(s)
+                
+            #    if s != 'mile' and s[len_s -1] == 'e':
+            #        s = s[:len_s - 1]
+
+            #m =  re.match(r'[+-]?\d+$', s)
+            #flag = 0
+            #for each in s :
+            #    if 
+
+            #if m is None :
+            #try :
+
+            #    eval(s)
+            #expect:
+            #if type(eval(s)) != float and type(eval(s)) != int :  
+            if s.isalpha(): 
+                #print s
+
+                unit_name = self._unit_dict.get(s,s)
+                eval_list[pos -1] = str(float(eval_list[pos - 1]) * self._legth_dict[unit_name]  )
+
+                eval_list[pos] = 'm'
+                #print eval_list[pos - 1]
+          
+        eval_str = ' '.join(eval_list)             
+        eval_str = eval_str.replace('m' ,' ')
+        #print eval_str
+        return  eval(eval_str)
+
+            
+
+if __name__ == '__main__':
+
+    test = Length('input.txt')
+    test.run()
+    
+    
+    test.compute('0.15 yard')
+     
+    print "done it"
